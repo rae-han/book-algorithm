b343a327f8be5fcd42ea22273553ddffce18db45
raehan <raehan900809@gmail.com>
2025-02-15 16:42:01 +0900

    feature: 5번 문제 풀이

--- src/coding-interview/array/1.ts
+++ src/coding-interview/array/1.ts
@@ -50,9 +50,16 @@ const solution_4_factory = (patterns) => (answers) => {
     score === maxScore ? result.concat(idx + 1) : result, []
   );
 };
-// 시간 여유가 있다면 patterns 값을 각각 무한 배열(제너레이터 이용)로 만들어서 문제를 풀 것.
+// 시간 여유가 있다면 patterns 값을 각각 무한 배열(제너레이터 이용)로 만들어서 인덱스 별로 하나씩 꺼내가며 문제를 풀 것.
+function* generator(array) {
+  while (true) { // 무한 반복
+    for (const value of array) {
+      yield value;
+    }
+  }
+}
 // 각 수포자를 배열로 넣어 남은 문제를 다 맞춰도 가장 높은 사람모다 못푼다면 배열에 넣지 않을 듯.
-// 근데 사실 문제에서 answers 값을 0부터 10,000 문제로 잡았기 때문에 대충 풀어도 다 됨.
+// 사실 문제에서 answers 값을 0부터 10,000 문제로 잡았기 때문에 대충 풀어도 다 됨. 어떤 회사냐에 따라 다르게 풀 듯.
 
 const solution_4 = solution_4_factory(patterns);
 
@@ -60,6 +67,22 @@ console.log(4, solution_4([1, 2, 3, 4, 5]));
 console.log(4, solution_4([1, 3, 2, 4, 2]));
 
 // 5. 행렬의 곱셈
+// const solution_5 = (arr1, arr2) => {
+//   return arr1.map(row => arr2.map((_, colIndex) => row.reduce((sum, value, i) => sum + value * arr2[i][colIndex], 0)));
+// }
+// 이런 방법도 있지만 행렬 곱셈은 패턴이 있어서 그냥 전치 행렬 만들어서 계산하게 할 듯.
+
+// 전치
+const transpose = (matrix) =>
+  matrix[0].map((_, colIndex) => matrix.map(row => row[colIndex]));
+// 내적
+const dot = (row, col) =>
+  row.reduce((sum, value, idx) => sum + value * col[idx], 0);
+const solution_5 = (arr1, arr2) => arr1.map(row => transpose(arr2).map(col => dot(row, col)));
+
+
+console.log(5, solution_5([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]));
+console.log(5, solution_5([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]));
 
 // 6. 실패율
 
