fea77ebc884ea127921f058391008afcf13817a1
raehan <raehan900809@gmail.com>
2025-02-15 14:12:24 +0900

    feature: 3번 문제 풀이

--- src/coding-interview/array/1.ts
+++ src/coding-interview/array/1.ts
@@ -17,7 +17,12 @@ console.log(2, solution_2([4, 2, 2, 1, 3, 4]));
 console.log(2, solution_2([2, 1, 1, 3, 2, 5, 4])); 
 
 // 3. 두 수를 뽑아서 더하기
+const solution_3: Solution = (numbers) => [...new Set(numbers.flatMap((x, i) => numbers.slice(0, i).map(y => x + y)))].sort((a, b) => a - b);
+// map, flat 함수로 나누는 것을 더 선호하지만 알고리즘 문제는 flatMap 함수를 주로 사용 함.
 
+console.log(3, solution_3([2, 1, 3, 4, 1]));
+console.log(3, solution_3([5, 0, 2, 7]));
+  
 // 4. 모의고사
 
 // 5. 행렬의 곱셈
